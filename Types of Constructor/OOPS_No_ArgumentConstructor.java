64fd77f8e90ef89a0a18c557e7d99aaf5a5bd3a0
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-08-09 16:36:29 +0500

    Create OOPS_No_ArgumentConstructor.java

---
+++ Types of Constructor/OOPS_No_ArgumentConstructor.java
@@ -0,0 +1,12 @@
+
+public class OOPS_No_ArgumentConstructor 
+{
+     OOPS_No_ArgumentConstructor()
+	    {
+			System.out.println(" No-argument Constructor.");
+	    }
+	    public static void main(String [] args)
+	    {
+			OOPS_No_ArgumentConstructor t=new OOPS_No_ArgumentConstructor();
+		}	
+}
