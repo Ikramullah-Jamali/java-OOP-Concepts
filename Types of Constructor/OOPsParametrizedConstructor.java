36ca1bc4e06abcf990e19218620d410582765168
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-08-09 16:38:40 +0500

    Add files via upload

---
+++ Types of Constructor/OOPsParametrizedConstructor.java
@@ -0,0 +1,17 @@
+
+class OOPsParametrizedConstructor 
+{
+
+    /* Parametrized Constructor */
+
+    OOPsParametrizedConstructor(int a) 
+    {
+        System.out.println(" Parametrized Constructor.");
+    }
+
+    public static void main(String[] args) 
+    {
+        OOPsParametrizedConstructor t = new OOPsParametrizedConstructor(10);
+    }
+
+}
