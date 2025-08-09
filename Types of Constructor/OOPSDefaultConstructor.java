a397fb2ac3cf48889b771d0c84c1db102375d349
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-08-09 16:38:07 +0500

    Add files via upload

---
+++ Types of Constructor/OOPSDefaultConstructor.java
@@ -0,0 +1,12 @@
+
+public class OOPSDefaultConstructor 
+{
+    //  Default Constructor  
+    
+	int i;
+    public static void main(String [] args)
+    {
+	   OOPSDefaultConstructor t=new OOPSDefaultConstructor();
+	   System.out.print("Default value of i: "+ t.i);
+    }     
+}
